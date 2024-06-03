efdcb29c41a02accada07a2a53fea612eab185e9
FelipeCastro2021 <felipe2021castro@gmail.com>
2024-06-03 13:58:52 -0300

    Eye open or closed

--- js/index.js
+++ js/index.js
@@ -2,9 +2,11 @@ const eye = document.querySelector('#eye');
 const input = document.querySelector('#psw');
 
 eye.addEventListener('click', e => {
-  if (input.getAttribute('type') === 'password') {
-    input.setAttribute('type', 'text');
+  if (input.type == 'password') {
+    input.type = 'text';
+    eye.src = '/assets/eye.svg';
   } else {
-    input.setAttribute('type', 'password');
+    input.type = 'password';
+    eye.src = '/assets/eye-closed.svg';
   }
 });
